4dc0f55eec20812d86d920901644f90667801b1f
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-03-22 16:49:42 +0200

    complete PYGAME usage

--- UI/GameScreen.py
+++ UI/GameScreen.py
@@ -3,38 +3,39 @@ from math import sqrt
 import threading
 import pygame as pg
 import time
-
-
-def draw_hex(surface, col_in, col_out, pos, a):
-    x, y = pos
-    points = [(x - a / 2, y - a * sqrt(3) / 2),
-              (x + a / 2, y - a * sqrt(3) / 2),
-              (x + a, y),
-              (x + a / 2, y + a * sqrt(3) / 2),
-              (x - a / 2, y + a * sqrt(3) / 2),
-              (x - a, y)]
-    pg.draw.polygon(surface, col_in, points)
-    pg.draw.polygon(surface, col_out, points, 4)
+import UI.Hexagon as Hexagon
 
 
 class Screen:
+    """
+    Class that represents the screen of the game.
+    """
     def __init__(self, board, hex_size=20, first_player_color="red", second_player_color="blue"):
+        """
+        Initialize the Screen class.
+        :param board: The board of the game.
+        :param hex_size: The size of the hexagons.
+        :param first_player_color: The color of the first player.
+        :param second_player_color: The color of the second player.
+        """
         self.update_board_flag = True
         self.hex_size = hex_size
         self.first_player_color = first_player_color
         self.second_player_color = second_player_color
         self.board = board
+        self.surface = None
+        self.matrix_of_hexagons = None
 
-    def render_board(self):
-
-        screen = pg.display.set_mode((1280, 720))
-        screen.fill(pg.Color("white"))
-
-        starting_pos = pg.Vector2(screen.get_width() / 2,
-                                  screen.get_height() / 2 - len(self.board) * self.hex_size * sqrt(3) / 2)
+    def create_hexagons_grid(self):
+        """
+        Create a matrix of hexagons.
+        :return: A matrix of hexagons.
+        """
+        matrix_of_hexagons = [[None for _ in range(len(self.board))] for _ in range(len(self.board))]
+        starting_pos = pg.Vector2(self.surface.get_width() / 2,
+                                  self.surface.get_height() / 2 - len(self.board) * self.hex_size * sqrt(3) / 2)
         point_in_matrix = [0, 0]
         counter_of_elements = 0
-        # base = 0
         reach_diagonal = False
         num_of_elements_in_row = 1
         for row in range(len(self.board) * 2 - 1):
@@ -44,14 +45,9 @@ class Screen:
                 center_x = starting_pos.x + (col - num_of_elements_in_row / 2) * self.hex_size * 3
                 center_y = starting_pos.y + row * self.hex_size * sqrt(3) / 2
 
-                if self.board[point_in_matrix[0]][point_in_matrix[1]] == 1:
-                    draw_hex(screen, pg.Color(self.first_player_color), pg.Color("black"), (center_x, center_y),
-                             self.hex_size)
-                elif self.board[point_in_matrix[0]][point_in_matrix[1]] == -1:
-                    draw_hex(screen, pg.Color(self.second_player_color), pg.Color("black"), (center_x, center_y),
-                             self.hex_size)
-                else:
-                    draw_hex(screen, pg.Color("white"), pg.Color("black"), (center_x, center_y), self.hex_size)
+                hexagon = Hexagon.Hexagon(self.surface, pg.Color("white"), pg.Color("black"), (center_x, center_y),
+                                          self.hex_size, (point_in_matrix[0], point_in_matrix[1]))
+                matrix_of_hexagons[point_in_matrix[0]][point_in_matrix[1]] = hexagon
 
                 if point_in_matrix[0] == 0 and not reach_diagonal:
                     point_in_matrix[0] = point_in_matrix[1] + 1
@@ -69,22 +65,37 @@ class Screen:
                 num_of_elements_in_row -= 1
             else:
                 num_of_elements_in_row += 1
-        pg.display.flip()
+        return matrix_of_hexagons
 
-    def update_board(self, board):
-        self.board = board
-        self.update_board_flag = True
+    def render_board(self):
+        """
+        Render the board on the screen.
+        """
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == 1:
+                    self.matrix_of_hexagons[i][j].col_in = pg.Color(self.first_player_color)
+                elif self.board[i][j] == -1:
+                    self.matrix_of_hexagons[i][j].col_in = pg.Color(self.second_player_color)
+                self.matrix_of_hexagons[i][j].draw()
 
     def run(self):
+        """
+        Run the game.
+        """
         pg.init()
+        screen = pg.display.set_mode((1280, 720))
+        self.surface = screen
+        pg.display.set_caption("Hexagon Game")
+        self.matrix_of_hexagons = self.create_hexagons_grid()
+        self.surface.fill(pg.Color("white"))
         running = True
         while running:
+            self.surface.fill(pg.Color("white"))
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-            if self.update_board_flag:
-                self.render_board()
-                self.update_board_flag = False
+            self.render_board()
             pg.display.flip()  # Update the display
             pg.time.Clock().tick(60)  # Cap the frame rate
         pg.quit()
@@ -92,6 +103,9 @@ class Screen:
 
 # Example of how to use the Screen class
 def main():
+    """
+    Example of how to use the Screen class.
+    """
     board_example = [[0 for _ in range(10)] for _ in range(10)]
     screen = Screen(board_example)
     screen_thread = threading.Thread(target=screen.run)
@@ -102,7 +116,7 @@ def main():
                 board_example[i][j] = -1
             else:
                 board_example[i][j] = 1
-            screen.update_board(board_example)
+            screen.board = board_example
             time.sleep(0.3)
     screen_thread.join()
 
---
+++ UI/Hexagon.py
@@ -0,0 +1,52 @@
+import pygame as pg
+from math import sqrt
+
+
+class Hexagon:
+    """
+    Class that represents a hexagon.
+    """
+
+    def __init__(self, surface, col_in, col_out, pos, radius, position_in_matrix=(0, 0)):
+        """
+        Initialize the Hexagon class.
+        :param surface: The surface (screen) to draw the hexagon on.
+        :param col_in: The color of the hexagon.
+        :param col_out: The color of the border of the hexagon.
+        :param pos: The position of the center of the hexagon.
+        :param radius: The radius (size) of the hexagon.
+        :param position_in_matrix: The position of the hexagon in the matrix.
+        """
+        self.surface = surface
+        self.col_in = col_in
+        self.col_out = col_out
+        self.pos = pos
+        self.radius = radius
+        self.position_in_matrix = position_in_matrix
+        self.clicked = False
+
+    def draw(self):
+        """
+        Draw the hexagon on the screen.
+        :return:  True if the mouse is clicked the hexagon.
+        """
+        action = False
+
+        x, y = self.pos
+        points = [(x - self.radius / 2, y - self.radius * sqrt(3) / 2),
+                  (x + self.radius / 2, y - self.radius * sqrt(3) / 2),
+                  (x + self.radius, y),
+                  (x + self.radius / 2, y + self.radius * sqrt(3) / 2),
+                  (x - self.radius / 2, y + self.radius * sqrt(3) / 2),
+                  (x - self.radius, y)]
+        pg.draw.polygon(self.surface, self.col_in, points)
+        pg.draw.polygon(self.surface, self.col_out, points, 4)
+
+        pos = pg.mouse.get_pos()
+        if points[0][0] < pos[0] < points[1][0] and points[0][1] < pos[1] < points[3][1]:
+            if pg.mouse.get_pressed()[0] and not self.clicked:
+                self.clicked = True
+                print(f"Clicked on hexagon {self.position_in_matrix}")
+        if pg.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        return action
