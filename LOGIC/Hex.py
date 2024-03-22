22786d7af76f366c93b75972da0674f01fee2b78
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-03-22 12:02:33 +0200

    adding documentation to the hex board class

--- LOGIC/Hex.py
+++ LOGIC/Hex.py
@@ -4,9 +4,12 @@ import torch.nn as nn
 import torch.nn.functional as f
 
 
+# noinspection GrazieInspection
 class HexBoard:
-    # This class is a representation of the Hex board. It has methods to make moves, check for a winner,
-    # and clone the board.
+    """
+    This class is a representation of the Hex board. It has methods to make moves, check for a winner,
+    and clone the board.
+    """
 
     EMPTY = 0
     BLACK = 1
@@ -14,15 +17,28 @@ class HexBoard:
     ONGOING = -17
 
     def __init__(self, board_size=11):
+        """
+        Initialize the Hex board.
+        :param board_size: Size of the board (default is 11 x 11).
+        """
         self.board_size = board_size
         self.board = np.zeros((board_size, board_size), dtype=int)
         self.current_player = self.BLACK
         self.game_history = []
 
     def other_player(self, player):
+        """
+        Switch between players, for inner use.
+        :param player: The current player.
+        :return:  The other player.
+        """
         return self.WHITE if player == self.BLACK else self.BLACK
 
     def clone(self):
+        """
+        Create a deep copy of the board, with the same position and history.
+        :return: A cloned instance of the board.
+        """
         copy = HexBoard(self.board_size)
         copy.current_player = self.current_player
         copy.board = np.copy(self.board)
@@ -30,11 +46,19 @@ class HexBoard:
         return copy
 
     def last_move(self):
+        """
+        Get the last move made in the game.
+        :return: Last move coordinates (row, col).
+        """
         if len(self.game_history) == 0:
             return None
         return self.game_history[-1]
 
     def make_move(self, move):
+        """
+        Make a move on the board if it is legal.
+        :param move: Move coordinates (row, col).
+        """
         row = move[0]
         col = move[1]
         if 0 <= row < self.board_size and 0 <= col < self.board_size and self.board[row][col] == 0:
@@ -43,18 +67,28 @@ class HexBoard:
             self.current_player = self.other_player(self.current_player)
 
     def unmake_move(self):
-        # unmake the last move
+        """
+        Undo the last move made in the game.
+        """
         if len(self.game_history) > 0:
             move = self.game_history.pop()
             self.board[move[0]][move[1]] = 0
             self.current_player = self.other_player(self.current_player)
 
     def legal_moves(self):
-        # return all legal moves available on the board
+        """
+        Get all legal moves available on the board.
+        :return: List of legal move coordinates (row, col).
+        """
         return [(i, j) for i in range(self.board_size) for j in range(self.board_size) if self.board[i][j] == 0]
 
     def get_neighbors(self, player, move):
-        # get the neighbors of a move
+        """
+        Get all the same color neighbors of a move to find winning path, for inner use.
+        :param player: The current player.
+        :param move: Move coordinates (row, col).
+        :return: List of neighbors coordinates (row, col).
+        """
         row = move[0]
         col = move[1]
         neighbors = []
@@ -85,7 +119,13 @@ class HexBoard:
         return neighbors
 
     def search_for_win_path(self, player, move):
-        # search for a winning path for the player
+        """
+        Search for a winning path from one side of the board to the other, for inner use.
+        This function implements some kind of DFS to find a path from one side to the other.
+        :param player: The player to check for a win path (makes the last move).
+        :param move: Move coordinates (row, col).
+        :return: True if a win path is found, False otherwise.
+        """
         visited = [[False for _ in range(self.board_size)] for _ in range(self.board_size)]
         visited[move[0]][move[1]] = True
         neighbors = self.get_neighbors(player, move)
@@ -106,7 +146,10 @@ class HexBoard:
         return False
 
     def check_outcome(self):
-        # determine the outcome of the game
+        """
+        Determine the outcome of the game.
+        :return: Outcome of the game (BLACK, WHITE, or ONGOING).
+        """
         if len(self.game_history) == 0:
             return self.ONGOING
 
@@ -118,7 +161,11 @@ class HexBoard:
         return self.ONGOING
 
     def __str__(self):
-        # string representation of the game board
+        """
+        String representation of the game board in the console.
+        This is use for example and testing purposes.
+        :return: The string representation of the game board.
+        """
         symbols = {self.EMPTY: '.', self.BLACK: 'X', self.WHITE: 'O'}
         board_str = ''
         for i in range(self.board_size):
